10392ef69762e552fa4856bd7fca203277404506
garniy <forbedlam@gmail.com>
2025-08-05 15:38:36 +0300

    f

---
+++ _nuxt/DwIDKQ__.js
@@ -0,0 +1,1 @@
+const t={common:{darkMode:{t:0,b:{t:2,i:[{t:3}],s:"Dark Mode"}},language:{t:0,b:{t:2,i:[{t:3}],s:"Language"}},settings:{t:0,b:{t:2,i:[{t:3}],s:"Settings"}}}};export{t as default};
---
+++ _nuxt/R_fNjJy0.js
@@ -0,0 +1,1 @@
+function s(e,n=4){return`${e.slice(0,n)}...${e.slice(-n)}`}export{s};
