c5b6fc4115acc521b1c8c5d170d9da819c6ffabc
garniy <forbedlam@gmail.com>
2025-08-05 10:49:13 +0300

    first commit

---
+++ _nuxt/CKUxJlT1.js
@@ -0,0 +1,1 @@
+const t={common:{darkMode:{t:0,b:{t:2,i:[{t:3}],s:"Темний режим"}},language:{t:0,b:{t:2,i:[{t:3}],s:"Мова"}},settings:{t:0,b:{t:2,i:[{t:3}],s:"Налаштування"}}}};export{t as default};
